5fc0cebb41ed5d64c2d805eb77176311d3cc8faa
aibaljose <aibaljosej@gmail.com>
2025-01-30 00:34:28 +0530

    acmajce

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
 
-import Home from "./home"
+import Home from "./Home"
 import Ticket from "./ticket"
 import Form from "./form"
 import Dashboard from "./dashboard"
